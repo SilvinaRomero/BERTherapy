3545c2648d91356b16d19df448bbae28fcf15e3f
SilvinaRomero <sromero.fernandez@gmail.com>
2025-11-01 19:06:14 +0100

    actualizar carga mejor modelo

--- bots/Patient.py
+++ bots/Patient.py
@@ -1,10 +1,21 @@
 from bots.Person import Person
+import os
+import json
+
+# Obtener la ruta del directorio del proyecto (BERTherapy)
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+# Obtener la configuración del terapeuta (guardada con optuna)
+with open(os.path.join(PROJECT_ROOT, "models", "config", "patient.json"), "r") as file:
+    config_patient = json.load(file)
+
 
 class Patient(Person):
     def __init__(self):
         # ruta del modelo, selección del pool de respuestas y número de clusters
-        self.dir_pool = "/home/silvina/proyectos/BERTherapy/data/processed/response_candidates_patient_full.txt"
-        self.dir_model = "/home/silvina/proyectos/BERTherapy/models/bert_patient_v1.0" # important! version a utilizar
+        self.dir_pool = os.path.join(PROJECT_ROOT, "data", "processed", "response_candidates_patient_full.txt")
+        self.dir_model = os.path.join(PROJECT_ROOT, "models", f"bert_patient_v{config_patient['version']}")
         super().__init__(self.dir_model, self.dir_pool, max_pool_size=10000, n_clusters=100)
 
     def respond(self, emotion, sentiment, context, input_text):
--- bots/Therapist.py
+++ bots/Therapist.py
@@ -1,10 +1,20 @@
 from bots.Person import Person
+import os
+import json
+
+# Obtener la ruta del directorio del proyecto (BERTherapy)
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Obtener la configuración del terapeuta (guardada con optuna)
+with open(os.path.join(PROJECT_ROOT, "models", "config", "therapist.json"), "r") as file:
+    config_therapist = json.load(file)
+
 
 class Therapist(Person):
     def __init__(self):
         # ruta del modelo, selección del pool de respuestas y número de clusters
-        self.dir_pool = "/home/silvina/proyectos/BERTherapy/data/processed/response_candidates_therapist_full.txt"
-        self.dir_model = "/home/silvina/proyectos/BERTherapy/models/bert_therapist_v1.0" # important! version a utilizar
+        self.dir_pool = os.path.join(PROJECT_ROOT, "data", "processed", "response_candidates_therapist_full.txt")
+        self.dir_model = os.path.join(PROJECT_ROOT, "models", f"bert_therapist_v{config_therapist['version']}")
         super().__init__(self.dir_model, self.dir_pool, max_pool_size=15000, n_clusters=100)
     
     def respond(self, emotion, sentiment, context, input_text):
